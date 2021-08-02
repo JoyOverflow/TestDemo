28f9d0c83825d6347bea994b4407068606779cde
joyoverflow <joyoverflow@sina.com>
2021-08-03 00:51:34 +0800

    first master

---
+++ src/MyMain.java
@@ -0,0 +1,11 @@
+public class MyMain {
+    public static void main(String[] args) {
+        char str='她';
+        String tmp= singleInt(str);
+        System.out.println(tmp);
+    }
+    public static String singleInt(char str) {
+        int result = str;
+        return String.valueOf(result);
+    }
+}
